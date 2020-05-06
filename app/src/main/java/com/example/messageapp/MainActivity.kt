ce07dfbd101a502a47890ed33d32d86fa07a9ada
park kyung suk <boardguy1024@gmail.com>
2020-05-06 23:52:33 +0900

    login画面作成

--- app/src/main/java/com/example/messageapp/MainActivity.kt
+++ app/src/main/java/com/example/messageapp/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.messageapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        register_button.setOnClickListener {
+            val email = email_textview.text.toString()
+            val password = password_textview.text.toString()
+
+            Log.d("MainActivity", "Email is: " + email)
+            Log.d("MainActivity", "Password: $password")
+        }
+
+        already_have_account_textview.setOnClickListener {
+            Log.d("MainActivity", "Try to show Login Activity")
+
+            // launch the login activity somehow
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
