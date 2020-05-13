d13eea5308e7670878656da714f4cb67df053a75
park kyung suk <boardguy1024@gmail.com>
2020-05-13 17:12:04 +0900

    Added FireabseAuth Rounded textView and buttons

--- app/src/main/java/com/example/messageapp/LoginActivity.kt
+++ app/src/main/java/com/example/messageapp/LoginActivity.kt
@@ -2,13 +2,31 @@ package com.example.messageapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.email_textview
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_login)
 
+        login_button.setOnClickListener {
+
+            val email = email_textview.text.toString()
+            val password = password_textview.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+
+                }
+
+        }
+
+        back_to_reg_textview.setOnClickListener {
+            finish()
+        }
     }
 }
